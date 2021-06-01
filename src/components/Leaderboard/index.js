f6ab6e85bb29b8b0f51e1c55bf112d5ab187cae8
Salik <salik.ansari6@gmail.com>
2021-06-01 16:28:31 +0530

    added loading indicators for fetch requests

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -1,55 +1,57 @@
-import React,{useEffect,useState} from 'react'
-import {Link} from 'react-router-dom'
-import './Leaderboard.css'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Preloader } from "react-preloader-tmnt";
+import "./Leaderboard.css";
 
 const Leaderboard = () => {
-    const [stats,setStats] = useState(null)
+  const [stats, setStats] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() =>{
-        fetch('https://word-race-backend.herokuapp.com/api/stats')
-        .then(res =>res.json())
-        .then(data => {
-            setStats(data)
-        }) 
-    },[])
+  useEffect(() => {
+    fetch("https://word-race-backend.herokuapp.com/api/stats")
+      .then((res) => res.json())
+      .then((data) => {
+        setStats(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
 
-    if(!stats){
-        return(
-        <div className="container">
-            <h1>Loading.....</h1>
-        </div>
-        )
-    }
+  return (
+    <div className="leaderboard">
+      <h1>Leaderboard</h1>
+      <table className="leaderboard__table">
+        <thead>
+          <tr>
+            <th>Sr. no</th>
+            <th>User</th>
+            <th>Score</th>
+            <th>Max Level</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stats?.map((stat, index) => {
+            return (
+              <tr key={stat._id}>
+                <td>{index + 1}</td>
+                <td>
+                  <Link to={`/user-stats/${stat.user._id}`}>
+                    {stat.user.name}
+                  </Link>
+                </td>
+                <td>{stat.score}</td>
+                <td>{stat.level}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <Preloader loaderType="dots" color="#3498db" loading={loading} />
+    </div>
+  );
+};
 
-    return (
-        <div className='leaderboard'>
-            <h1>Leaderboard</h1>
-            <table className="leaderboard__table">
-                <thead>
-                    <tr>
-                        <th>Sr. no</th>
-                        <th>User</th>
-                        <th>Score</th>
-                        <th>Max Level</th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    stats?.map(((stat,index) =>{
-                        return (
-                            <tr key={stat._id}>
-                                <td>{index+1}</td>
-                                <td><Link to={`/user-stats/${stat.user._id}`}>{stat.user.name}</Link></td>
-                                <td>{stat.score}</td>
-                                <td>{stat.level}</td>
-                            </tr>
-                        )
-                    }))
-                }
-                </tbody>  
-            </table>
-        </div>
-    )
-}
-
-export default Leaderboard
+export default Leaderboard;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,82 +1,124 @@
-import React,{useState,useRef,useEffect} from 'react'
-import './SignUp.css'
+import React, { useState, useRef, useEffect } from "react";
+import { Preloader } from "react-preloader-tmnt";
+import "./SignUp.css";
+
 const SignUp = () => {
-    const [errors,setErrors] = useState('')
-    const [form,setForm] = useState({name:"",email:'',password:'',confirmPassword:''})
-    const [passwordAlert,setPasswordAlert] = useState(false)
-    const [userCreatedAlert,setUserCreatedAlert] = useState(false)
+  const [errors, setErrors] = useState("");
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [passwordAlert, setPasswordAlert] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [userCreatedAlert, setUserCreatedAlert] = useState(false);
 
-    const handleChange = e =>{
-        setUserCreatedAlert('')
-        setForm({...form,[e.target.name]:e.target.value})
-        
+  const handleChange = (e) => {
+    setUserCreatedAlert("");
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const confirmPassRef = useRef(null);
+
+  useEffect(() => {
+    if (document.activeElement === confirmPassRef.current) {
+      if (form.password !== form.confirmPassword) {
+        setPasswordAlert(true);
+      } else {
+        setPasswordAlert(false);
+      }
     }
-    const confirmPassRef = useRef(null)
-    
-    useEffect(() =>{        
-        if(document.activeElement === confirmPassRef.current){
-            if(form.password !== form.confirmPassword){
-                   setPasswordAlert(true)
-               }
-               else{
-                   setPasswordAlert(false)
-               }
+  }, [form]);
+
+  const handleSubmit = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    fetch("https://word-race-backend.herokuapp.com/api/user/register", {
+      method: "POST",
+      body: JSON.stringify({
+        name: form.name,
+        email: form.email,
+        password: form.password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw res.json();
         }
-       },[form])
+      })
+      .then((userId) => {
+        setLoading(false);
+        setErrors("");
+        setUserCreatedAlert(true);
+        setForm({ name: "", email: "", password: "", confirmPassword: "" });
+      })
+      .catch((err) => {
+        setLoading(false);
+        err.then((err) => setErrors(err.message));
+      });
+  };
 
-
-    const handleSubmit = e =>{
-        e.preventDefault()
-        fetch('https://word-race-backend.herokuapp.com/api/user/register',{
-            method:"POST",
-            body:JSON.stringify({
-                name:form.name,
-                email:form.email,
-                password:form.password
-            }),
-            headers:{
-                'Content-Type':'application/json'
-            }
-        })
-        .then(res => {
-            if(res.ok){
-                return res.json()
-            }
-            else{
-                throw res.json()
-            }
-        })
-        .then(userId =>{
-            setErrors("")
-            setUserCreatedAlert(true)
-            setForm({name:"",email:'',password:'',confirmPassword:''})
-
-        })
-        .catch(err =>{ 
-            err.then(err => setErrors(err.message))
-        })        
-    }
-
-    return (
-        <div className="sign-up">
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input required onChange={handleChange} value={form.name} type="text" name="name" id="name"/>
-                <label htmlFor="email">Email</label>
-                <input required onChange={handleChange} value={form.email} type="text" name="email" id="email"/>
-                <label htmlFor="password" name="password">Password</label>
-                <input required onChange={handleChange} value={form.password} type="password" name="password" id="password"/>
-                <label htmlFor="confirm-password">Confirm Password</label>
-                <input required onChange={handleChange} value={form.confirmPassword} type="password" ref={confirmPassRef} name="confirmPassword" id="confirm-password"/>
-                {
-                    passwordAlert && <span className="confirm-alert" >Passwords do not match</span>
-                }
-                <button type="submit">Sign Up</button>
-            </form>
-            {errors && <div className="errors">{errors}</div>}
-            {userCreatedAlert && <div className="user-created-success">User Created Successfully. Please go to the Login Page and continue</div>}
+  return (
+    <div className="sign-up">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          required
+          onChange={handleChange}
+          value={form.name}
+          type="text"
+          name="name"
+          id="name"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          required
+          onChange={handleChange}
+          value={form.email}
+          type="text"
+          name="email"
+          id="email"
+        />
+        <label htmlFor="password" name="password">
+          Password
+        </label>
+        <input
+          required
+          onChange={handleChange}
+          value={form.password}
+          type="password"
+          name="password"
+          id="password"
+        />
+        <label htmlFor="confirm-password">Confirm Password</label>
+        <input
+          required
+          onChange={handleChange}
+          value={form.confirmPassword}
+          type="password"
+          ref={confirmPassRef}
+          name="confirmPassword"
+          id="confirm-password"
+        />
+        {passwordAlert && (
+          <span className="confirm-alert">Passwords do not match</span>
+        )}
+        <button type="submit">Sign Up</button>
+        <Preloader loading={loading} spinnerSize="50" />
+      </form>
+      {errors && <div className="errors">{errors}</div>}
+      {userCreatedAlert && (
+        <div className="user-created-success">
+          User Created Successfully. Please go to the Login Page and continue
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default React.memo(SignUp)
+export default React.memo(SignUp);
