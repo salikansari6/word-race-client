320d7b9c3723e42ecffb7a710495b121467d398d
Salik Ansari <salik.ansari6@gmail.com>
2021-06-08 21:24:00 +0530

    fix for issue#6

--- src/components/Keyboard/index.js
+++ src/components/Keyboard/index.js
@@ -1,87 +1,113 @@
-import React,{useState,useEffect} from 'react'
-import "./Keyboard.css"
-import {sound} from '../../assets/sounds'
+import React, { useState, useEffect } from "react";
+import "./Keyboard.css";
+import { sound } from "../../assets/sounds";
 
+const Keyboard = ({ input, setInput, showGameOver }) => {
+  const [activeKey, setActiveKey] = useState("");
+  const inputRef = React.useRef();
+  const [showKeyboard, setShowKeyboard] = useState(false);
+  const screenWidth = window.innerWidth;
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
 
-const Keyboard = ({input,setInput,showGameOver}) => {
-    const [activeKey,setActiveKey] = useState("")
-    const inputRef = React.useRef()
-    const [showKeyboard,setShowKeyboard] = useState(false)
-    
-    const screenWidth = window.innerWidth
-
-    useEffect(() =>{
-            if(screenWidth > 800){
-                setShowKeyboard(true)
-            }
-        },[screenWidth])
-
-
-    const handleChange = e =>{
-        if(e.key === " "){
-            e.preventDefault();
-        }
-        setInput(e.target.value)
+  useEffect(() => {
+    if (screenWidth > 800) {
+      setShowKeyboard(true);
     }
+  }, [screenWidth]);
 
-
-    const handleKeyUp = e =>{
-        setActiveKey("")
+  const handleChange = (e) => {
+    if (e.key === " ") {
+      e.preventDefault();
     }
+    setInput(e.target.value);
+  };
 
-    const handleKeyDown = e =>{
-        if(e.key === " "){
-            e.preventDefault();
-        }
-        setActiveKey(e.key)
-        sound.play()
+  const handleKeyUp = (e) => {
+    setActiveKey("");
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === " ") {
+      e.preventDefault();
     }
+    setActiveKey(e.key);
+    sound.play();
+  };
 
-    return (
-        <div className="keyboard">
-            <input type="text" value={input}
-                autoCapitalize="none"
-                className="keyboard-input" 
-                autocomplete="off" 
-                spellcheck="false" 
-                autocorrect="off"
-                onChange={handleChange} 
-                placeholder="Type here..."
-                disabled={showGameOver}
-                ref={inputRef}
-                onKeyDown={handleKeyDown}
-                onKeyUp={handleKeyUp}
-                />
-            {
-                showKeyboard &&  <div className="keyboard-wrapper">
-                <div className="first-row">{"qwertyuiop".split("").map((letter,index) =>{
-                    return <div className="key" key={letter}
-                    style={{
-                        backgroundColor:activeKey === letter ? "#fed34e": null,
-                        transform : activeKey === letter ? 'scale(1.2)' : null
-                    }}>{letter}</div>
-                })}
+  return (
+    <div className="keyboard">
+      <input
+        type="text"
+        value={input}
+        autoCapitalize="none"
+        className="keyboard-input"
+        autocomplete="off"
+        spellcheck="false"
+        autocorrect="off"
+        onChange={handleChange}
+        placeholder="Type here..."
+        disabled={showGameOver}
+        ref={inputRef}
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
+      />
+      {showKeyboard && (
+        <div className="keyboard-wrapper">
+          <div className="first-row">
+            {"qwertyuiop".split("").map((letter, index) => {
+              return (
+                <div
+                  className="key"
+                  key={letter}
+                  style={{
+                    backgroundColor: activeKey === letter ? "#fed34e" : null,
+                    transform: activeKey === letter ? "scale(1.2)" : null,
+                  }}
+                >
+                  {letter}
                 </div>
-                <div className="second-row">{"asdfghjkl".split("").map((letter,index) =>{
-                    return <div className="key" key={letter}
-                    style={{
-                        backgroundColor:activeKey === letter ? "#fed34e": null,
-                        transform : activeKey === letter ? 'scale(1.2)' : null
-                    }}>{letter}</div>
-                })}</div>
-                <div className="third-row">{"zxcvbnm".split("").map((letter,index) =>{
-                    return <div className="key" key={letter}
-                    style={{
-                        backgroundColor:activeKey === letter ? "#fed34e": null,
-                        transform : activeKey === letter ? 'scale(1.2)' : null
-                    }}>{letter}</div>
-                })}</div>
-            </div>
-            }
-           
+              );
+            })}
+          </div>
+          <div className="second-row">
+            {"asdfghjkl".split("").map((letter, index) => {
+              return (
+                <div
+                  className="key"
+                  key={letter}
+                  style={{
+                    backgroundColor: activeKey === letter ? "#fed34e" : null,
+                    transform: activeKey === letter ? "scale(1.2)" : null,
+                  }}
+                >
+                  {letter}
+                </div>
+              );
+            })}
+          </div>
+          <div className="third-row">
+            {"zxcvbnm".split("").map((letter, index) => {
+              return (
+                <div
+                  className="key"
+                  key={letter}
+                  style={{
+                    backgroundColor: activeKey === letter ? "#fed34e" : null,
+                    transform: activeKey === letter ? "scale(1.2)" : null,
+                  }}
+                >
+                  {letter}
+                </div>
+              );
+            })}
+          </div>
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Keyboard
+export default Keyboard;
