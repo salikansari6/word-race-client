99c814660205fe5d6b2e679364a38759152b76c9
Salik Ansari <salik.ansari6@gmail.com>
2021-06-14 12:41:12 +0530

    added lazy imports(code splitting) for route components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
-import React,{useState,useEffect} from 'react'
-import "./App.css"
-import Game from './components/Game'
-import StartScreen from './components/StartScreen'
-import Sidebar from './components/Sidebar'
-import Login from './components/Login'
-import Leaderboard from './components/Leaderboard'
-import SignUp from './components/SignUp'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import UserStats from './components/UserStats'
+import React, { useState, useEffect, Suspense } from "react";
+import "./App.css";
+import { LineProgress } from "react-preloader-tmnt";
+import Sidebar from "./components/Sidebar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-const App = () =>{
+const Game = React.lazy(() => import("./components/Game"));
+const StartScreen = React.lazy(() => import("./components/StartScreen"));
+const Leaderboard = React.lazy(() => import("./components/Leaderboard"));
+const SignUp = React.lazy(() => import("./components/SignUp"));
+const UserStats = React.lazy(() => import("./components/UserStats"));
+const Login = React.lazy(() => import("./components/Login"));
 
-    const [isAuth,setIsAuth] = useState(JSON.parse(localStorage.getItem('isAuth')) || false)
-    useEffect(() =>{
-        localStorage.setItem('isAuth',isAuth)
-    },[isAuth])
+const App = () => {
+  const [isAuth, setIsAuth] = useState(
+    JSON.parse(localStorage.getItem("isAuth")) || false
+  );
+  useEffect(() => {
+    localStorage.setItem("isAuth", isAuth);
+  }, [isAuth]);
 
-    return (
-       <div className="App" id="App">
-        <Router>
-                <Sidebar  isAuth={isAuth}  setIsAuth={setIsAuth}/>
-                <Switch>
-                    <Route component={() =>(
-                        <Login setIsAuth={setIsAuth} isAuth={isAuth}/>
-                    )} exact path='/login'></Route>
-                    <Route component={SignUp} exact path='/signup'></Route>
-                    <Route component={Leaderboard} exact path="/leaderboard"></Route>
-                    <Route component={StartScreen} exact path="/"></Route>
-                    <Route component={()=>(<Game isAuth={isAuth}/>)} exact path="/game"></Route>
-                    <Route component={()=>(<UserStats isAuth={isAuth}/>)} exact path="/user-stats/:id?"></Route>
-                </Switch>
-            </Router>
-       </div>
+  return (
+    <div className="App" id="App">
+      <Router>
+        <Sidebar isAuth={isAuth} setIsAuth={setIsAuth} />
+        <Switch>
+          <Suspense fallback={<LineProgress loading={true} />}>
+            <Route
+              component={() => <Login setIsAuth={setIsAuth} isAuth={isAuth} />}
+              exact
+              path="/login"
+            ></Route>
+            <Route component={SignUp} exact path="/signup"></Route>
+            <Route component={Leaderboard} exact path="/leaderboard"></Route>
+            <Route component={StartScreen} exact path="/"></Route>
+            <Route
+              component={() => <Game isAuth={isAuth} />}
+              exact
+              path="/game"
+            ></Route>
+            <Route
+              component={() => <UserStats isAuth={isAuth} />}
+              exact
+              path="/user-stats/:id?"
+            ></Route>
+          </Suspense>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
-   )
-}
-
-export default App
+export default App;
